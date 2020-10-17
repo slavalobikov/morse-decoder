1aecea6a9fb9c6355fed9b6238a7f3ed025d6b45
lobik <lobikova12@mail.ru>
2020-10-17 22:28:15 +0300

    fix: add

--- src/index.js
+++ src/index.js
@@ -38,8 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+
+    arr = arr.map(el => el.slice(el.indexOf(1)));
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === '*') {
+            arr[i] = ' ';
+
+        } else {
+            let temp = ''
+            for (let j = 0; j < arr[i].length; j += 2) {
+                let letter = arr[i].slice(j, j + 2);
+                (letter === '10') ? temp = temp.concat('.'): temp = temp.concat('-')
+            }
+            arr[i] = temp;
+        }
+
+    }
+
+
+    arr = arr.map(el => (el === ' ')? el = ' ': el = MORSE_TABLE[el]);
+
+
+    return arr.join('');}
 
 module.exports = {
     decode
